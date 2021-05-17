9b0df368ec8e16c9afbf48a9b2b0fe8a85efbfad
alicjazurko <58707380+alicjazurko@users.noreply.github.com>
2021-05-17 22:05:08 +0200

    Add files via upload

--- kurs.py
+++ kurs.py
@@ -1,58 +1,25 @@
 import time as t
-from matplotlib import gridspec
 import pandas as pd
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
+import math
 
-from matplotlib.animation import FuncAnimation
-from itertools import count
-from matplotlib import animation, rc
-from IPython.display import HTML
+from matplotlib import gridspec
+from matplotlib import animation
 
-msft = yf.Ticker("MSFT")
+
+tic = "MSFT"
+msft = yf.Ticker(tic)
 info = msft.info
+pd.set_option("display.max_rows", None)
 df = pd.DataFrame.from_dict(info, orient='index', columns=['value'])
-df.head(20)
+df.head(123)
 
 # tabela kursów historycznych
 hist = msft.history(period="max")
 # print(hist)
-
-# średnia 200-sesyjna
-hist = msft.history(period="max")
-x = hist.index
-y1 = hist["Close"]
-y2 = y1.rolling(200).mean()
-plt.figure(figsize=(12, 6))
-plt.plot(x, y1, y2)
-plt.yscale('log')
-plt.grid(True)
-# plt.show()
-
-# wykres przefiltrowany od 2008 200 wynikow
-
-m = 200
-year = 2008
-
-hist = msft.history(period="max")
-x = hist.index  # data
-y1 = hist["Close"]  # close
-y2 = y1.rolling(m).mean()  # średnia 200 wyników
-
-filt = hist.index >= dt.datetime(year, 1, 1)
-# hist = hist[filt]
-x = x[filt]
-y1 = y1[filt]
-y2 = y2[filt]
-
-plt.figure(figsize=(12, 6))
-plt.plot(x, y1, y2)
-plt.yscale('log')
-plt.grid(True)
-# plt.show()
-
 
 # wykres przefiltrowany od 2008 200 wynikow z volume i średnią m-sesyjną
 m = 200
@@ -85,8 +52,20 @@ ax2.grid(True)
 # ax1.axes.xaxis.set_ticklabels([])
 plt.setp(ax1.get_xticklabels(), visible=False)
 plt.subplots_adjust(hspace=0)  # ściśnięcie wykresów
-# plt.show()
 
+
+# prezentacja w tabeli wybranych wskaźników
+
+multiples = {"longName": "Company name", "marketCap": "market capitalisation", "trailingAnnualDividendYield":
+             "Dividend yield (12 months)", "payoutRatio": "Payout Ratio", "trailingPE": "P/E", "forwardPE": "forward P/E", "enterpriseToEbitda": "EV/EBITDA", "priceToBook": "P/BV", "sharesPercentSharesOut": "shares short ratio"}
+dic = {}
+
+for i in range(len(multiples)):
+    value = df.loc[list(multiples.keys())[i]].value
+    dic[list(multiples.values())[i]] = value
+mdf = pd.DataFrame.from_dict(dic, orient='index', columns=['Multiples'])
+mdf
+print(mdf)
 # Wyświetlanie w czasie rzeczywistym co sekundę ostatniego kursu akcji
 
 # pobierana jest najmniejsza możliwa paczka danych (z ostatniego dnia co minutę) i pobierany jest z niej ostatni kurs
@@ -106,16 +85,14 @@ def current_price(tic):
 
 
 fig, ax = plt.subplots()
-tic = "MSFT"
+
 current_price = yf.download(tickers=tic, period="1d", interval="1m")
 close_price = current_price["Close"]
 date = current_price.index  # index dataframe, ostatni dzień kursu
-print(current_price)
-print(date)
 
-t = np.arange(date[0], date[-1], len(date))
 
-ax = plt.axis([date[0], date[-1], min(close_price) - 1, max(close_price) + 1])
+ax = plt.axis([date[0], date[0] + dt.timedelta(hours=8),
+               min(close_price) - 1, max(close_price) + 1])
 
 line, = plt.plot(date, close_price)
 
@@ -124,17 +101,11 @@ def animate(i):
     current_price = yf.download(tickers=tic, period="1d", interval="1m")
     close_price = current_price["Close"]
     date = current_price.index  # index dataframe, ostatni dzień kursu
-    # t = np.arange(date[0], date[-1], len(date))
-    # print("czujka", t)
-    # print("czujka", date)
     line.set_data(date, close_price)
     return (line,)
 
 
-# create animation using the animate() function
 myAnimation = animation.FuncAnimation(
     fig, animate, interval=1000, blit=True, repeat=False)
 
 plt.show()
-
-# print("czujka111111", animate(1))
